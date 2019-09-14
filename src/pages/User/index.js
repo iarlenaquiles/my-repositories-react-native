7412d533a4c9d81635eb7ef7e3ac6db81f4a3ead
Iarlen Aquiles <iarlensilva775@gmail.com>
2019-09-14 15:54:52 -0300

    começando a estilização da lista de stars do user

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
 import PropTypes from 'prop-types';
 import api from '../../services/api';
-// import { Container } from './styles';
+import { Container, Header, Avatar, Name, Bio } from './styles';
 
 export default class User extends Component {
   static navigationOptions = ({ navigation }) => ({
@@ -32,6 +31,17 @@ export default class User extends Component {
 
   render() {
     const { stars } = this.state;
-    return <View />;
+    const { navigation } = this.props;
+    const user = navigation.getParam('user');
+
+    return (
+      <Container>
+        <Header>
+          <Avatar source={{ uri: user.avatar }} />
+          <Name>{user.name}</Name>
+          <Bio>{user.bio}</Bio>
+        </Header>
+      </Container>
+    );
   }
 }
---
+++ src/pages/User/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View``;
+
+export const Header = styled.View``;
+
+export const Avatar = styled.Image``;
+
+export const Name = styled.Text``;
+
+export const Bio = styled.Text``;
